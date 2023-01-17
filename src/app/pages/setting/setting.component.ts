85984af7d53fb529e97cb620d1201910bc992a1b
ITJacob <jacob.wd8816@gmail.com>
2023-01-17 15:38:59 +0800

    1 file modified

--- src/app/pages/setting/setting.component.ts
+++ src/app/pages/setting/setting.component.ts
@@ -6,13 +6,35 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./setting.component.css'],
 })
 export class SettingComponent {
-  convertFile(e) {
-    const reader = new FileReader();
+  async convertFile(e) {
     const file = e.files[0];
+    const result = await this.readFile(file);
+    const lines = (result as string).split('\r\n');
+    const skills = this.getSkills(lines);
+    console.log(skills);
+  }
 
-    reader.readAsText(file);
-    reader.onloadend = () => {
-      console.log(reader.result);
-    };
+  readFile(file) {
+    return new Promise((res) => {
+      const reader = new FileReader();
+      reader.readAsText(file);
+      reader.onloadend = () => {
+        res(reader.result);
+      };
+    });
+  }
+
+  getSkills(lines) {
+    const header: any[] = lines[0]
+      .split(',')
+      .filter((l) => l)
+      .map((l) => l.split('-')[1]);
+    const content: any[] = lines.slice(1).map((l) => l.split(','));
+    return content.map((c) =>
+      header.reduce((pre, cur, i) => {
+        if (cur) pre[cur] = c[i];
+        return pre;
+      }, {})
+    );
   }
 }
