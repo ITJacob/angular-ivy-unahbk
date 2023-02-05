bed5e23b7699475ae74272b278c019d256682cbd
ITJacob <jacob.wd8816@gmail.com>
2023-02-05 10:08:56 +0800

    1 file modified

--- src/app/control/gameManager.ts
+++ src/app/control/gameManager.ts
@@ -1,12 +1,12 @@
+import { BehaviorSubject } from 'rxjs';
 import { IUserData } from '../interface/IData';
 import { getData } from '../mock/mockData';
 import { checkInfo } from '../mock/mockInfo';
-import { Skill } from '../model/skill';
 import { Behavior } from './behavior';
-import { HeroControl } from './heroControl';
 import { Player } from './player';
 
 export class GameManager {
+  eventBus: BehaviorSubject<{ type: string; content?: any }>;
   player: Player;
   match: Player;
   behaviors: Behavior[] = [];
@@ -25,18 +25,20 @@ export class GameManager {
     // get match 信息
     const matchData = await getData<IUserData>('USER', matchId);
     this.match = new Player(matchData.heros);
-
+    this.eventBus = new BehaviorSubject({ type: 'start' });
     this.newRound();
   }
 
   private newRound() {
     this.player.beforeRound();
     this.match.beforeRound();
+    this.eventBus.next({ type: 'newRound' });
   }
 
   private endRound() {
     // TODO: check winner
     this.isPlaying = false;
+    this.eventBus.next({ type: 'endRound' });
   }
 
   private beforeStart() {
@@ -44,15 +46,20 @@ export class GameManager {
     // TODO: waiting match operate
     this.behaviors = [...this.player.behaviors, ...this.match.behaviors];
     // TODO: order behaviors
+    this.eventBus.next({ type: 'start' });
   }
 
   start() {
     this.beforeStart();
     this.behaviors.forEach((b) => {
-      b.active([this.player.heroControls, this.match.heroControls]);
-      b.actor.buffCheck();
+      this.operate(b);
     });
     this.endRound();
     this.newRound();
+  }
+
+  async operate(b: Behavior) {
+    b.active([this.player.heroControls, this.match.heroControls]);
+    b.actor.buffCheck();
   }
 }
